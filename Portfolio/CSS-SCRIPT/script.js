54a28c5d1d1b5551461dd0d0a0da3041e77e2e2e
VandanaTripathi31 <tripathivandana086@gmail.com>
2024-03-04 00:59:28 +0530

    first commit

---
+++ Portfolio/CSS-SCRIPT/script.js
@@ -0,0 +1,40 @@
+$(document).ready(function () {
+    $(window).scroll(function () {
+        if (this.scrollY > 20) {
+            $('.navbar').addClass("sticky");
+        } else {
+            $('.navbar').removeClass("sticky");
+        }
+
+    })
+//    typing animation using typed js
+var typed = new Typed(".typing", {
+    strings: ["C.E. Student","Web Developer" ,"FreeLancer"  ],
+    typeSpeed: 100,
+    backSpeed:60,
+    loop:true
+});
+
+});
+ 
+
+// About me section
+
+var tablinks = document.getElementsByClassName("tab-links");
+var tabcontents = document.getElementsByClassName("tab-contents");
+
+function opentab(tabname) {
+    for (tablink of tablinks) {
+        tablink.classList.remove("active-link");
+    }
+
+    for (tabcontent of tabcontents) {
+        tabcontent.classList.remove("active-tab");
+    }
+
+    event.currentTarget.classList.add("active-link");
+    document.getElementById(tabname).classList.add("active-tab");
+}
+
+
+
